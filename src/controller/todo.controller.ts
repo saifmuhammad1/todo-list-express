6506705bc5f6f7a006d879f8d126e532a5d20cf9
saifmuhammad1 <pokemonsaif786@gmail.com>
2025-12-04 14:26:43 +0530

    upadted module

--- src/controller/todo.controller.ts
+++ src/controller/todo.controller.ts
@@ -4,26 +4,33 @@ export const createTodo = async (req: Request, res: Response) => {
   try {
     console.log("req.body:", req.body);
     const todo = await Todo.create(req.body);
-    return res.status(201).json({ message: "Todo created ", todo });
+    return res.status(200).json({ message: "Todo created ", todo });
   } catch (error) {
-    console.log("th error is ", error);
-    res.status(500).json({ error });
+    console.error("th error is ", error);
+    return res.status(500).json({ error });
   }
 };
 
 export const getTodoList = async (req: Request, res: Response) => {
   try {
-    // const page = req.query.page || 1;
-    // const limit = req.query.limit || 10;
+    const total = await Todo.countDocuments();
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
 
-    // const startIndex = (Number(page) - 1) * Number(limit);
+    const startIndex = (Number(page) - 1) * Number(limit);
 
-    const todo = await Todo.find();
-    // .skip(startIndex).limit(Number(limit));
-    return res.status(200).json({ result: [...todo] });
+    const todo = await Todo.find().skip(startIndex).limit(Number(limit));
+    const updatedObject = todo.map((item) => ({
+      ...item.toObject(),
+      page,
+      limit,
+      total,
+    }));
+
+    return res.status(200).json({ result: updatedObject });
   } catch (error) {
-    console.log("th error is ", error);
-    res.status(500).json({ error });
+    console.error("th error is ", error);
+    return res.status(500).json({ error });
   }
 };
 
@@ -33,10 +40,10 @@ export const getTodoById = async (req: Request, res: Response) => {
     if (!id) return res.status(400).json({ error: "ID not found" });
     console.log(id);
     const todo = await Todo.findById(id);
-    return res.status(201).json({ todo });
+    return res.status(200).json({ todo });
   } catch (error) {
-    console.log("th error is ", error);
-    res.status(500).json({ error });
+    console.error("th error is ", error);
+    return res.status(500).json({ error });
   }
 };
 
@@ -45,13 +52,14 @@ export const updateTodoData = async (req: Request, res: Response) => {
     // const id = req.query.id as string;
     // if (!id) return res.status(400).json({ error: "ID not found" });
     // console.log(id);
-    const todo = await Todo.findByIdAndUpdate(req.body.ID, req.body, {
+    console.log(req.body, req.body._id);
+    const todo = await Todo.findByIdAndUpdate(req.body._id, req.body, {
       new: true,
     });
-    return res.status(201).json({ message: "Todo Updated", todo });
+    return res.status(200).json({ message: "Todo Updated", todo });
   } catch (error) {
-    console.log("th error is ", error);
-    res.status(500).json({ error });
+    console.error("th error is ", error);
+    return res.status(500).json({ error });
   }
 };
 
@@ -61,9 +69,9 @@ export const deleteTodoData = async (req: Request, res: Response) => {
     if (!id) return res.status(400).json({ error: "ID not found" });
     console.log(id);
     const todo = await Todo.findByIdAndDelete(id);
-    return res.status(201).json({ message: "Deleted", todo });
+    return res.status(200).json({ message: "Deleted", todo });
   } catch (error) {
-    console.log("th error is ", error);
-    res.status(500).json({ error });
+    console.error("th error is ", error);
+    return res.status(500).json({ error });
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
@@ -22,7 +16,7 @@ connectDB();
 
 app.use("/api/todos", todoRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
